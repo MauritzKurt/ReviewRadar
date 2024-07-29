d63d3d5da8cd3be9bc3b9cf6f1558f516e363ac2
Kurt Mauritz <38671493+MauritzKurt@users.noreply.github.com>
2024-07-29 16:55:17 +0000

    fixed navbar, added styling to user's profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root 'homepage#home'
-
+  
   devise_for :users
-
+  get ':username/reviews' => 'reviews#index', as: :current_user_reviews
+  
   resources :reviews
   resources :products
   resources :companies
