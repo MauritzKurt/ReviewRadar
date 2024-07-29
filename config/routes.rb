4114318e7f2a6d8814da3c367e5f4be74512b98a
Kurt Mauritz <38671493+MauritzKurt@users.noreply.github.com>
2024-07-29 21:23:31 +0000

    added initial css to products/companies

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   
   devise_for :users
   get ':username/reviews' => 'reviews#index', as: :current_user_reviews
+
+  # get ':products' => 'products#index', as: :products_index
+  # get ':companies' => 'companies#index', as: :companies_index
   
   resources :reviews
   resources :products
