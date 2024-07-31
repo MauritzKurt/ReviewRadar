d9ac0b7c8379d6210d718816eec703a2d174431f
Kurt Mauritz <38671493+MauritzKurt@users.noreply.github.com>
2024-07-31 16:33:49 +0000

    added description to company model

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,12 +2,13 @@
 #
 # Table name: companies
 #
-#  id         :bigint           not null, primary key
-#  logo_image :string           default("logo_placeholder.png")
-#  name       :string
-#  url        :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id          :bigint           not null, primary key
+#  description :text
+#  logo_image  :string           default("logo_placeholder.png")
+#  name        :string
+#  url         :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
 #
 class Company < ApplicationRecord
   has_many :products, class_name: 'Product', foreign_key: 'company_id', dependent: :destroy
